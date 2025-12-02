309cc8fc0f971442ad7802ce58685f676d20d056
Roy Le <roy.le@viindoo.com>
2025-12-02 16:10:17 +0700

    [FIX] web_editor, mail, account_edi: add indexes to improve performance when deleting ir_attachment records
    On a DB with 15M+ attachments, deleting a single attachment takes
    several seconds. Most of the time is spent on the circular `original_id`
    and `message_main_attachment_id` foreign key.

--- addons/mail/__manifest__.py
+++ addons/mail/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Discuss',
-    'version': '1.10',
+    'version': '1.10.1',
     'category': 'Productivity/Discuss',
     'sequence': 145,
     'summary': 'Chat, mail gateway and private channels',
